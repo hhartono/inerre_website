dc19b6b262567b548bfd91dc537fd58d6ea7b2e6
Nugraha S. Fajar <nugie.nsf@gmail.com>
2016-10-12 16:39:30 +0700

    portfolio upload

--- application/models/modelportfolio.php
+++ application/models/modelportfolio.php
@@ -17,10 +17,28 @@ class Modelportfolio extends CI_Model {
 		}
 	}
 
-	public function insertPortfolio($title, $description, $slugtitle)
+	public function loadPortfolioProjectId($uri){
+        $this->db->select('portfolio_project.*');
+        $this->db->from('portfolio_project');
+        $this->db->where('portfolio_id', $uri);
+        $query = $this->db->get();
+
+        return $query->row_array();
+    }
+
+	public function insertPortfolioId($id)
+	{
+		$field = array(
+			'portfolio_id' => $id
+		);
+		$this->db->insert('portfolio_project', $field);
+	}
+
+	public function insertPortfolio($title, $description, $location, $slugtitle)
 	{
 		$field = array(
 			'portfolio_title' => $title,
+			'portfolio_location' => $location,
 			'portfolio_description' => $description,
 			'portfolio_uri' => $slugtitle
 		);
@@ -37,10 +55,50 @@ class Modelportfolio extends CI_Model {
 		$this->db->insert('portfolio_album', $field);
 	}
 
-	public function updatePortfolio($idportfolio, $title, $description, $slugtitle)
+	public function insertPortfolioProject($photo, $id)
+	{
+		$field = array(
+			'photo1' => $photo
+		);
+		$this->db->where('portfolio_id', $id);
+		$this->db->update('portfolio_project', $field);
+	}
+
+	public function insertPortfolioProjectPhoto2($photo, $id)
+	{
+		$field = array(
+			'photo2' => $photo
+		);
+		$this->db->where('portfolio_id', $id);
+		$this->db->update('portfolio_project', $field);
+	}
+
+	public function insertPortfolioDescription($highlights1, $highlights2, $description_left, $description_right, $idportfolio)
+	{
+		$field = array(
+			'highlights1' => $highlights1,
+			'highlights2' => $highlights2,
+			'description_left' => $description_left,
+			'description_right' => $description_right
+		);
+		$this->db->where('portfolio_id', $idportfolio);
+		$this->db->update('portfolio_project', $field);
+	}
+
+	public function insertPortfolioCarousel($photo, $idportfolio)
+	{
+		$field = array(
+			'photo' => $photo,
+			'portfolio_id' => $idportfolio
+		);
+		$this->db->insert('portfolio_carousel', $field);
+	}
+
+	public function updatePortfolio($idportfolio, $title, $location, $description, $slugtitle)
 	{
 		$field = array(
 			'portfolio_title' => $title,
+			'portfolio_location' => $location,
 			'portfolio_description' => $description,
 			'portfolio_uri' => $slugtitle
 		);
@@ -60,6 +118,54 @@ class Modelportfolio extends CI_Model {
 					LIMIT 0,1
 				) AS photo_portfolio
 			FROM portfolio p 
+			ORDER BY p.id DESC
+		");
+		if($query->num_rows() > 0){
+			foreach ($query->result() as $row) {
+				$data[] = $row;
+			}
+			return $data;
+		}
+	}
+
+	public function loadPortfolioProject($uri)
+	{
+		$query = $this->db->query("
+			SELECT p.*, pp.photo1 as photo1, pp.photo2 as photo2, pp.highlights1 as description1, pp.highlights2 as description2
+			FROM portfolio p, portfolio_project pp
+			where p.id = pp.portfolio_id AND pp.portfolio_id = '$uri' 
+			ORDER BY p.id DESC
+		");
+		if($query->num_rows() > 0){
+			foreach ($query->result() as $row) {
+				$data[] = $row;
+			}
+			return $data;
+		}
+	}
+
+	public function loadPortfolioCarousel($uri)
+	{
+		$query = $this->db->query("
+			SELECT p.*, pc.photo as photo
+			FROM portfolio p, portfolio_carousel pc
+			WHERE p.id = pc.portfolio_id AND pc.portfolio_id = '$uri' 
+			ORDER BY p.id DESC
+		");
+		if($query->num_rows() > 0){
+			foreach ($query->result() as $row) {
+				$data[] = $row;
+			}
+			return $data;
+		}
+	}
+
+	public function loadPortfolioDescription($uri)
+	{
+		$query = $this->db->query("
+			SELECT p.*, pp.description_right as description_right, pp.description_left as description_left
+			FROM portfolio p, portfolio_project pp
+			where p.id = pp.portfolio_id AND pp.portfolio_id = '$uri' 
 			ORDER BY p.id DESC
 		");
 		if($query->num_rows() > 0){
--- application/views/public/portfolio.php
+++ application/views/public/portfolio.php
@@ -32,11 +32,25 @@
                     ?>
                              <!-- Photo Item -->
                             <div class="col-md-6 col-lg-6 mb-md-10">
-                                <div class="post-prev-img">
+                                <div class="post-prev-img" style="margin-bottom:60px;">
                                     <a href="/upload/portfolio/<?php echo $lp->photo_portfolio;?>" class="lightbox-gallery-2 mfp-image">
                                         <img src="/upload/portfolio/<?php echo $lp->photo_portfolio;?>" alt="INERRE BANDUNG portfolios" />
                                     </a>
                                 </div>
+                                <!-- <div class="alt-features-item align-center">
+                                    <div class="alt-features-descr align-left">
+                                        <h2 class="section-title mt-0 font-alt align-left" style="margin-top:-50px !important; margin-bottom:-14px;"><?php echo $lp->portfolio_title; ?></h2>
+                                        <h3 class="alt-features-title font-alt" style="color:#00010C; margin-bottom:14px;"><?php echo $lp->portfolio_location;?></h3>
+                                        <?php
+                                            $description =  $lp->portfolio_description;
+                                            $karakter = 433;
+                                            $tampil = substr($description, 0, $karakter);
+                                            echo $tampil;
+                                        ?>
+                                    </div>
+                                </div>
+                                </br>
+                                </br> -->
                             </div>
                             <!-- End Photo Item -->
                     <?php
@@ -44,74 +58,166 @@
                     ?>
 
                         <!-- Photo Item
-                        <div class="col-md-6 col-lg-6 mb-md-10">
+                        <div class="col-md-4 col-lg-4 mb-md-10">
                             <div class="post-prev-img">
                                 <a href="/assets/images/portfolio-inerre/015532-1140.jpg" class="lightbox-gallery-2 mfp-image"><img src="/assets/images/portfolio-inerre/015532-650.jpg" alt="" /></a>
                             </div>
+                            <div class="alt-features-item align-center">
+                                <div class="alt-features-descr align-left">
+                                    <h2 class="section-title mt-0 font-alt align-left" style="margin-top:-50px !important; margin-bottom:-14px;">Title 1</h2>
+                                    <h3 class="alt-features-title font-alt" style="color:#00010C; margin-bottom:14px;">Bandung, Indonesia</h3>
+                                    From idea to installation, from concept to check-in, we want to make sure that all of our designs are able to reflect the characters of our clients’ personalities and lifestyles, from the broad narrative down to the smallest most meaningful details.
+                                </div>
+                            </div>
+                            </br>
+                            </br>
                         </div>
                         End Photo Item
                         
                         Photo Item
-                        <div class="col-md-6 col-lg-6 mb-md-10">
+                        <div class="col-md-4 col-lg-4 mb-md-10">
                             <div class="post-prev-img">
                                 <a href="/assets/images/portfolio-inerre/025053-1140.jpg" class="lightbox-gallery-2 mfp-image"><img src="/assets/images/portfolio-inerre/025053-650.jpg" alt="" /></a>
                             </div>
+                            <div class="alt-features-item align-center">
+                                <div class="alt-features-descr align-left">
+                                    <h2 class="section-title mt-0 font-alt align-left" style="margin-top:-50px !important; margin-bottom:-14px;">Title 2</h2>
+                                    <h3 class="alt-features-title font-alt" style="color:#00010C; margin-bottom:14px;">Bandung, Indonesia</h3>
+                                    From idea to installation, from concept to check-in, we want to make sure that all of our designs are able to reflect the characters of our clients’ personalities and lifestyles, from the broad narrative down to the smallest most meaningful details.
+                                </div>
+                            </div>
+                            </br>
+                            </br>
                         </div>
                         End Photo Item
                         Photo Item
-                        <div class="col-md-6 col-lg-6 mb-md-10">
+                        <div class="col-md-4 col-lg-4 mb-md-10">
                             <div class="post-prev-img">
                                 <a href="/assets/images/portfolio-inerre/034836-1140.jpg" class="lightbox-gallery-2 mfp-image"><img src="/assets/images/portfolio-inerre/034836-650.jpg" alt="" /></a>
                             </div>
+                            <div class="alt-features-item align-center">
+                                <div class="alt-features-descr align-left">
+                                    <h2 class="section-title mt-0 font-alt align-left" style="margin-top:-50px !important; margin-bottom:-14px;">Title 3</h2>
+                                    <h3 class="alt-features-title font-alt" style="color:#00010C; margin-bottom:14px;">Bandung, Indonesia</h3>
+                                    From idea to installation, from concept to check-in, we want to make sure that all of our designs are able to reflect the characters of our clients’ personalities and lifestyles, from the broad narrative down to the smallest most meaningful details.
+                                </div>
+                            </div>
+                            </br>
+                            </br>
                         </div>
                         End Photo Item
                         Photo Item
-                        <div class="col-md-6 col-lg-6 mb-md-10">
+                        <div class="col-md-4 col-lg-4 mb-md-10">
                             <div class="post-prev-img">
                                 <a href="/assets/images/portfolio-inerre/034909-1140.jpg" class="lightbox-gallery-2 mfp-image"><img src="/assets/images/portfolio-inerre/034909-650.jpg" alt="" /></a>
                             </div>
+                            <div class="alt-features-item align-center">
+                                <div class="alt-features-descr align-left">
+                                    <h2 class="section-title mt-0 font-alt align-left" style="margin-top:-50px !important; margin-bottom:-14px;">Title 4</h2>
+                                    <h3 class="alt-features-title font-alt" style="color:#00010C; margin-bottom:14px;">Bandung, Indonesia</h3>
+                                    From idea to installation, from concept to check-in, we want to make sure that all of our designs are able to reflect the characters of our clients’ personalities and lifestyles, from the broad narrative down to the smallest most meaningful details.
+                                </div>
+                            </div>
+                            </br>
+                            </br>
                         </div>
                         End Photo Item
                         Photo Item
-                        <div class="col-md-6 col-lg-6 mb-md-10">
+                        <div class="col-md-4 col-lg-4 mb-md-10">
                             <div class="post-prev-img">
                                 <a href="/assets/images/portfolio-inerre/045216-1140.jpg" class="lightbox-gallery-2 mfp-image"><img src="/assets/images/portfolio-inerre/045216-650.jpg" alt="" /></a>
                             </div>
+                            <div class="alt-features-item align-center">
+                                <div class="alt-features-descr align-left">
+                                    <h2 class="section-title mt-0 font-alt align-left" style="margin-top:-50px !important; margin-bottom:-14px;">Title 5</h2>
+                                    <h3 class="alt-features-title font-alt" style="color:#00010C; margin-bottom:14px;">Bandung, Indonesia</h3>
+                                    More than 80% of your customers care about social impact. That's a lot of people. It's also why sustainable brands are great and why positive branding is vital. You want your customers to trust you better than the competition.
+                                    Our positive branding process involves creating and designing sustainable brands by connecting you with innovative nonprofits who can help you discover how you can maximise social impact in your community.
+                                </div>
+                            </div>
+                            </br>
+                            </br>
                         </div>
                         End Photo Item
                         Photo Item
-                        <div class="col-md-6 col-lg-6 mb-md-10">
+                        <div class="col-md-4 col-lg-4 mb-md-10">
                             <div class="post-prev-img">
                                 <a href="/assets/images/portfolio-inerre/051453-1140.jpg" class="lightbox-gallery-2 mfp-image"><img src="/assets/images/portfolio-inerre/051453-650.jpg" alt="" /></a>
                             </div>
+                           <div class="alt-features-item align-center">
+                                <div class="alt-features-descr align-left">
+                                    <h2 class="section-title mt-0 font-alt align-left" style="margin-top:-50px !important; margin-bottom:-14px;">Title 6</h2>
+                                    <h3 class="alt-features-title font-alt" style="color:#00010C; margin-bottom:14px;">Bandung, Indonesia</h3>
+                                    From idea to installation, from concept to check-in, we want to make sure that all of our designs are able to reflect the characters of our clients’ personalities and lifestyles, from the broad narrative down to the smallest most meaningful details.
+                                </div>
+                            </div>
+                            </br>
+                            </br>
                         </div>
                         End Photo Item
                         Photo Item
-                        <div class="col-md-6 col-lg-6 mb-md-10">
+                        <div class="col-md-4 col-lg-4 mb-md-10">
                             <div class="post-prev-img">
                                 <a href="/assets/images/portfolio-inerre/052712-1140.jpg" class="lightbox-gallery-2 mfp-image"><img src="/assets/images/portfolio-inerre/052712-650.jpg" alt="" /></a>
                             </div>
+                            <div class="alt-features-item align-center">
+                                <div class="alt-features-descr align-left">
+                                    <h2 class="section-title mt-0 font-alt align-left" style="margin-top:-50px !important; margin-bottom:-14px;">Title 7</h2>
+                                    <h3 class="alt-features-title font-alt" style="color:#00010C; margin-bottom:14px;">Bandung, Indonesia</h3>
+                                    More than 80% of your customers care about social impact. That's a lot of people. It's also why sustainable brands are great and why positive branding is vital. You want your customers to trust you better than the competition.
+                                    Our positive branding process involves creating and designing sustainable brands by connecting you with innovative nonprofits who can help you discover how you can maximise social impact in your community.
+                                </div>
+                            </div>
+                            </br>
+                            </br>
                         </div>
                         End Photo Item
                         Photo Item
-                        <div class="col-md-6 col-lg-6 mb-md-10">
+                        <div class="col-md-4 col-lg-4 mb-md-10">
                             <div class="post-prev-img">
                                 <a href="/assets/images/portfolio-inerre/052786-1140.jpg" class="lightbox-gallery-2 mfp-image"><img src="/assets/images/portfolio-inerre/052786-650.jpg" alt="" /></a>
                             </div>
+                            <div class="alt-features-item align-center">
+                                <div class="alt-features-descr align-left">
+                                    <h2 class="section-title mt-0 font-alt align-left" style="margin-top:-50px !important; margin-bottom:-14px;">Title 8</h2>
+                                    <h3 class="alt-features-title font-alt" style="color:#00010C; margin-bottom:14px;">Bandung, Indonesia</h3>
+                                    From idea to installation, from concept to check-in, we want to make sure that all of our designs are able to reflect the characters of our clients’ personalities and lifestyles, from the broad narrative down to the smallest most meaningful details.
+                                </div>
+                            </div>
+                            </br>
+                            </br>
                         </div>
                         End Photo Item
                         Photo Item
-                        <div class="col-md-6 col-lg-6 mb-md-10">
+                        <div class="col-md-4 col-lg-4 mb-md-10">
                             <div class="post-prev-img">
                                 <a href="/assets/images/portfolio-inerre/059594-1140.jpg" class="lightbox-gallery-2 mfp-image"><img src="/assets/images/portfolio-inerre/059594-650.jpg" alt="" /></a>
                             </div>
+                            <div class="alt-features-item align-center">
+                                <div class="alt-features-descr align-left">
+                                    <h2 class="section-title mt-0 font-alt align-left" style="margin-top:-50px !important; margin-bottom:-14px;">Title 9</h2>
+                                    <h3 class="alt-features-title font-alt" style="color:#00010C; margin-bottom:14px;">Bandung, Indonesia</h3>
+                                    From idea to installation, from concept to check-in, we want to make sure that all of our designs are able to reflect the characters of our clients’ personalities and lifestyles, from the broad narrative down to the smallest most meaningful details.
+                                </div>
+                            </div>
+                            </br>
+                            </br>
                         </div>
                         End Photo Item
                         Photo Item
-                        <div class="col-md-6 col-lg-6 mb-md-10">
+                        <div class="col-md-4 col-lg-4 mb-md-10">
                             <div class="post-prev-img">
                                 <a href="/assets/images/portfolio-inerre/059982-1140.jpg" class="lightbox-gallery-2 mfp-image"><img src="/assets/images/portfolio-inerre/059982-650.jpg" alt="" /></a>
                             </div>
+                            <div class="alt-features-item align-center">
+                                <div class="alt-features-descr align-left">
+                                    <h2 class="section-title mt-0 font-alt align-left" style="margin-top:-50px !important; margin-bottom:-14px;">Title 10</h2>
+                                    <h3 class="alt-features-title font-alt" style="color:#00010C; margin-bottom:14px;">Bandung, Indonesia</h3>
+                                    From idea to installation, from concept to check-in, we want to make sure that all of our designs are able to reflect the characters of our clients’ personalities and lifestyles, from the broad narrative down to the smallest most meaningful details.
+                                </div>
+                            </div>
+                            </br>
+                            </br>
                         </div>
                         End Photo Item -->
                     <?php
@@ -128,7 +234,7 @@
             <!-- End Section -->
             
             <!-- Contact Section -->
-            <section class="page-section" id="contact">
+            <section class="page-section" id="contact" style="margin-top:-200px !important;">
                 <div class="container relative">
                     <h2 class="section-title font-alt mb-70 mb-sm-40">
                         Find Inerre
